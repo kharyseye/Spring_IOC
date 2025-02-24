ba2e55a08b358b68d11545cfdc4b223c6f944843
KHARY_SEYE <mamekharyseye380@gmail.com>
2025-02-24 18:29:05 +0000

    Instanciation static V1

--- SpringIOC/src/main/java/com/irritec/presentation/Presentation1.java
+++ SpringIOC/src/main/java/com/irritec/presentation/Presentation1.java
@@ -1,7 +1,14 @@
 package com.irritec.presentation;
 
+import com.irritec.dao.DaoImpl;
+import com.irritec.metier.MetierImpl;
+
 public class Presentation1 {
     public static void main(String[] args) {
-        
+        MetierImpl metier = new MetierImpl();
+        DaoImpl dao = new DaoImpl();
+        metier.setDao(dao);
+        System.out.println("- - - - - - - - - - - - - - - LA PREMIERE VERSION- - - - - - - - - - - - - - ");
+        System.out.println(metier.calcul());
     }
 }
